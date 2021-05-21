4fecd2e09d151b33f7d8df6eb1b9bfd6d00fbaae
YisusGo <agusdipo04@gmail.com>
2021-05-20 22:30:19 -0300

    band socket server

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,7 +1,17 @@
 const { io }= require("../index");
+const Band = require("./models/band");
+const Bands = require("./models/bands");
+
+const bands = new Bands();
+bands .addBand(new Band("duku"));
+bands .addBand(new Band("messi"));
+bands .addBand(new Band("cr7"));
+
 //mensajes de sockets
 io.on('connection', client => {
-    console.log("Cliente conectado")
+    console.log("Cliente conectado");
+
+    client.emit("active bands",bands.getBands());
 
     client.on('disconnect', () => {
         console.log("Cliente desconectado")
@@ -9,8 +19,27 @@ io.on('connection', client => {
 
     client.on("msj", function (payload) {
         console.log("Escuchando..", payload);
+        io.emit("msj", { quien: "io" });
 
+    });
+    
+    client.on("mensajeDesdeApp", (payload)=>{
+        console.log(payload);
+        io.emit("reenvio",payload);
+    });
 
-    })
-    io.emit("msj", { quien: "io" });
+    client.on ("vote band", (payload)=> {
+        bands.voteBand(payload.id);
+        io.emit("active bands", bands.getBands());
+    });
+    client.on("addBand",(payload)=>{
+        const newBand=new Band(payload.name);
+        bands.addBand(newBand);
+        io.emit("active bands", bands.getBands());
+    });
+
+    client.on("deleteBand",(payload)=>{
+        bands.deleteBand(payload.id);
+        io.emit("active bands", bands.getBands());
+    });
 });
